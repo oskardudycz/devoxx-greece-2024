98fa5c5a7ee14641f7c1b346485783f17b14152a
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-04-16 15:36:20 +0200

    Added NonNegativeInt VO

--- src/main/java/io/eventdriven/slimdownaggregates/original/infrastructure/validation/Validation.java
+++ src/main/java/io/eventdriven/slimdownaggregates/original/infrastructure/validation/Validation.java
@@ -11,6 +11,11 @@ public final class Validation {
       throw new IllegalArgumentException("Number has to be positive");
   }
 
+  public static void assertNonNegative(int number) {
+    if(number < 0)
+      throw new IllegalArgumentException("Number cannot be negative");
+  }
+
   public static void assertNotEmpty(String text) {
     assertNotNull(text);
 
